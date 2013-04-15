aa36818fae1b67797b1af91ddb8d97e26766488d
Hio Leong <Home@dhcp-34-65.xlate.eduroam.ucl.ac.uk>
2013-04-15 16:26:54 +0100

    Added and Implemented TupleSeqNode class

---
+++ cwAST/ast/TupleSeqNode.java
@@ -0,0 +1,11 @@
+package ast;
+
+/**
+ * For this class, an interface would not make much sense any more. ;)
+ */
+public class TupleSeqNode extends SeqNode {
+
+	public TupleExprNode(List<ExprNode> exprNodes) {
+    	super(exprNode);
+    }
+}
