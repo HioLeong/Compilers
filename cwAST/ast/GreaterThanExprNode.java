a02088a259167028d94a74f1c053f71c4ccfc53b
Hio Leong <Home@dhcp-34-65.xlate.eduroam.ucl.ac.uk>
2013-04-15 18:23:07 +0100

    Added IfElseStmtNode class

--- cwAST/ast/GreaterThanExprNode.java
+++ cwAST/ast/GreaterThanExprNode.java
@@ -4,6 +4,7 @@ package ast;
  * For this class, an interface would not make much sense any more. ;)
  */
 public abstract class GreaterThanExprNode extends BinaryExprNode {
+	
     public GreaterThanExprNode(ExprNode lhs, TermNode rhs) {
         super(lhs,rhs);
     }
---
+++ cwAST/ast/ListSeqNode.java
@@ -0,0 +1,8 @@
+package ast;
+
+public class ListSeqNode extends Node {
+
+    public ListSeqNode(ExprList exprList) {
+       	super(exprList);
+    }
+}
