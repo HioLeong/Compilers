3ef58195b0390ffadd95fdaa59bfb384951c3b89
Hio Leong <Home@dhcp-34-65.xlate.eduroam.ucl.ac.uk>
2013-04-15 16:16:14 +0100

    Added SeqNode

---
+++ cwAST/ast/SeqNode.java
@@ -0,0 +1,11 @@
+package ast;
+
+/**
+ * For this class, an interface would not make much sense any more. ;)
+ */
+public abstract class SeqNode extends Node {
+
+	public SeqNode(TermNode lhs, FactorNode rhs) {
+        
+    }
+}
