851635eed9da15e306e0055a1273e8645a9ff488
Tina Fallahi <tina.fallahi.11@ucl.ac.uk>
2013-04-19 23:13:03 +0100

    Rasheed's 2 extra files

---
+++ cwAST/ast/BlockNode.java
@@ -0,0 +1,18 @@
+package ast;
+
+import java.util.List;
+
+public class BlockNode extends Node {
+
+	protected LocalDeclListNode ldl;
+	protected StmtListNode sl;
+    public BlockNode(LocalDeclListNode ldl, StmtListNode sl) {
+    	this.ldl=ldl;
+    	this.sl=sl;
+    }  
+    public BlockNode(StmtListNode sl) {
+    	this.sl=sl;
+    }  
+
+
+}
