a52287746b92d0c0eb4517d825607c57ce83dc95
Hio Leong <Home@dhcp-34-65.xlate.eduroam.ucl.ac.uk>
2013-04-20 15:36:58 +0100

    Fixed Syntactic Errors

--- cwAST/ast/DataTypeDeclNode.java
+++ cwAST/ast/DataTypeDeclNode.java
@@ -6,9 +6,9 @@ public class DataTypeDeclNode extends GlobalDeclNode {
 
 	protected ParameterListNode pl;
 
-    public DataTypeDeclNode(ParameterListNode pl) {
-    	this.pl=pl;
-    }  
+	public DataTypeDeclNode(ParameterListNode pl) {
+		this.pl=pl;
+	}  
 
 
 }
