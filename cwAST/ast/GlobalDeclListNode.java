1453894944d2c1e5a2c2bae5e1900890ac5c1911
Rasheed Wihaib <rasheedwihaib@gmail.com>
2013-04-20 19:04:03 +0100

    tester files/ Parsing complete

--- cwAST/ast/GlobalDeclListNode.java
+++ cwAST/ast/GlobalDeclListNode.java
@@ -7,9 +7,6 @@ public class GlobalDeclListNode extends Node {
 	public List<GlobalDeclNode> statements;
 
 	public GlobalDeclListNode(List<GlobalDeclNode> statements) {
-		for (GlobalDeclNode s : statements) {
-			System.out.println(s);
-		}
 		this.statements = statements;
 	}
 
