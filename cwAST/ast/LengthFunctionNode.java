902ef6445dd6f1ddeb2b432b9bdca42394598abc
Hio Leong <Home@dhcp-34-65.xlate.eduroam.ucl.ac.uk>
2013-04-20 18:38:30 +0100

    Added LengthFunctionNode

---
+++ cwAST/ast/LengthFunctionNode.java
@@ -0,0 +1,11 @@
+package ast;
+
+public class LengthFunctionNode extends Node {
+
+	private ExprNode exprNode;
+
+	public LengthFunctionNode(ExprNode exprNode) {
+		this.exprNode = exprNode;
+	}
+
+}
