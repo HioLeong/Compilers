23dd308da3213f2c7b52403e4f982d2fb9a456fd
Rasheed Wihaib <rasheedwihaib@gmail.com>
2013-04-20 18:31:41 +0100

    4

--- cwAST/ast/InExprNode.java
+++ cwAST/ast/InExprNode.java
@@ -1,9 +1,6 @@
 package ast;
 
-/**
- * For this class, an interface would not make much sense any more. ;)
- */
-public abstract class InExprNode extends BinaryExprNode {
+public class InExprNode extends BinaryExprNode {
     public InExprNode(ExprNode lhs, TermNode rhs) {
         super(lhs,rhs);
     }
--- cwAST/ast/OrExprNode.java
+++ cwAST/ast/OrExprNode.java
@@ -1,9 +1,6 @@
 package ast;
 
-/**
- * For this class, an interface would not make much sense any more. ;)
- */
-public abstract class OrExprNode extends BinaryExprNode {
+public class OrExprNode extends BinaryExprNode {
     public OrExprNode(ExprNode lhs, TermNode rhs) {
         super(lhs,rhs);
     }
