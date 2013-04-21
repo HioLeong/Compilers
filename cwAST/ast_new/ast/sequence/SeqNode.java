84b4f7695c23d5967b735ff5e1f4a2a7e1a46da1
Tina Fallahi <tina.fallahi.11@ucl.ac.uk>
2013-04-21 23:02:53 +0100

    StringSeqNode fixed

--- cwAST/ast_new/ast/sequence/SeqNode.java
+++ cwAST/ast_new/ast/sequence/SeqNode.java
@@ -7,10 +7,4 @@ import ast.expression.ExprListNode;
  * For this class, an interface would not make much sense any more. ;)
  */
 public abstract class SeqNode extends Node {
-
-	public ExprListNode exprList;
-
-	public SeqNode(ExprListNode exprList) {
-		this.exprList = exprList;
-	}
 }
