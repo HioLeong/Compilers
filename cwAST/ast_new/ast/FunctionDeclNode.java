4e0a7bc8992ff74b1cea2ec5b81870496ad9700c
Hio Leong <hiowengleong@gmail.com>
2013-04-22 00:21:12 +0100

    Changed FunctionDeclNode

--- cwAST/ast_new/ast/FunctionDeclNode.java
+++ cwAST/ast_new/ast/FunctionDeclNode.java
@@ -4,21 +4,22 @@ import visitor.Visitor;
 
 public class FunctionDeclNode extends GlobalDeclNode {
 
+	public String id;
 	public ParameterListNode pl;
 	public String ft;
 	public BlockNode b;
-	
-    public FunctionDeclNode(ParameterListNode pl, String ft,BlockNode b) {
-    	this.b=b;
-    	this.ft=ft;
-    	this.pl=pl;
-    }
+
+	public FunctionDeclNode(String id, ParameterListNode pl, String ft, BlockNode b) {
+		this.id = id;
+		this.b = b;
+		this.ft = ft;
+		this.pl = pl;
+	}
 
 	@Override
 	public Object accept(Visitor visitor) {
 		// TODO Auto-generated method stub
 		return null;
-	}  
-
+	}
 
 }
