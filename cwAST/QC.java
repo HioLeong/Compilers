4f2a2ad47ccd9dabde9cc3209d758c211ad2982e
Tina Fallahi <tina.fallahi.11@ucl.ac.uk>
2013-04-22 03:59:18 +0100

    compiling

--- cwAST/QC.java
+++ cwAST/QC.java
@@ -1,8 +1,14 @@
 import java.io.FileInputStream;
+
 import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.IOException;
 import java.io.InputStream;
 import java_cup.runtime.Symbol;
+import ast.*;
+import visitor.*;
 
 public class QC {
 	private static Lexer lex;
@@ -35,19 +41,19 @@ public class QC {
             return lexOnly;
         }
 
-	public static void main(String[] args) {
-		try {
-                    boolean lexOnly = processCommandLine(args);
-                    lex = new Lexer(input);
-                    pccParser = new parser(lex);
-                    if (lexOnly) {
-                        lexicalAnalysisOnly();
-                    } else {
-                        pccParser.parse();
-                    }
-                    } catch  (Exception ex) {
-			ex.printStackTrace();
-			System.exit(1);
-		}
+	public static void main(String[] args) throws FileNotFoundException {
+		Lexer myScanner = new Lexer(new BufferedReader(new FileReader(args[0])));
+		parser myParser = new parser(myScanner);
+	try{
+		Symbol result= myParser.parse();
+		Node root = (Node)result.value;
+		SementicVisitor sv = new SementicVisitor();
+		root.accept(sv);
+		System.out.println("--------------------------------------------------------------------------");
+	} catch(Exception e){
+		e.printStackTrace();
+		System.out.println("\n - Error in processing P File.");
+		//myParser.parseSuccessReport(false);
+	}
 	}
 }
